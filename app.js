e105e6566c46ceea11b1c9fce5e1aa3ee96d1bc8
pablothecoder <ortegaperaltap@gmail.com>
2024-11-09 23:25:16 -0400

    html css and  js work

---
+++ app.js
@@ -0,0 +1,8 @@
+const verificationCode = [3, 4, 8, 4]
+
+const digits = document.querySelector('.otp-digits')
+
+const digitOne = document.querySelector('#digit-one')
+const digitTwo = document.querySelector('#digit-two')
+const digitThree = document.querySelector('#digit-three')
+const digitFour = document.querySelector('#digit-four')
